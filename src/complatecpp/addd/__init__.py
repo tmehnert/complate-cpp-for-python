f03b59910dd6ffc8e37ffda51d3eb0551b847b07
Torsten Mehnert <torsten.mhn@gmail.com>
2021-12-30 11:55:17 +0100

    maybe in another .py directory?

--- src/complatecpp/addd/__init__.py
+++ src/complatecpp/addd/__init__.py
@@ -11,6 +11,4 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-from .core import Value, Function, Stream, StringStream, Renderer
-from .addd import *
-from .quickjs import QuickJsRenderer, QuickJsRendererBuilder
+from .reevaluatingrenderer import ReEvaluatingRenderer
